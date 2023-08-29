7476fdd36174c13acaba55cbe491d93aa96aa22b
McLinWxl <2018903135@chd.edu.cn>
2023-08-30 01:13:22 +0800

    RealtimeDrawing

--- realtime_drawing.py
+++
@@ -1,8 +0,0 @@
-from plotool import RealtimeDrawing
-
-dp = RealtimeDrawing(num_lines=2, style=["science", "ieee", "grid"])
-for i in range(50):
-    dp.add_data(i, i ** 2, "label1", 1)
-    dp.add_data(i, i, "label2", 2)
-    dp.plot_data()
-dp.show()
